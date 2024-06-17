b12168f18689db5e57d272942de453a30ab52fa0
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-17 23:47:08 +0200

    Init FirebaseAuthentication App

---
+++ FirebaseAuthentication/FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by Marc on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
