a0f19e8aa9833b2d795ee767b4570387442a947c
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-20 14:16:47 +0200

    Add InputView component

---
+++ FirebaseAuthentication/FirebaseAuthentication/Components/InputView.swift
@@ -0,0 +1,39 @@
+//
+//  InputView.swift
+//  FirebaseAuthentication
+//
+//  Created by Marc on 20/06/2024.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    
+    let title: String
+    let placeholder: String
+    var isSecureField: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            } else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    InputView(text: .constant(""), title: "Email Address", placeholder: "johndoe@protonmail.com")
+}
