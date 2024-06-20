aa1cbbbc4e7169ce141d64febb7d4edc2fd815ee
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-20 16:43:46 +0200

    Init RegisterView

---
+++ FirebaseAuthentication/FirebaseAuthentication/Core/Authentication/RegisterView.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterView.swift
+//  FirebaseAuthentication
+//
+//  Created by Marc on 20/06/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        VStack {
+            Image(.firebaseLogo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120)
+                .padding(.vertical, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    RegisterView()
+}
