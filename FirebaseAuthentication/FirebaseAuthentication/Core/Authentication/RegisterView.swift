13d58be8025d19ea058ee7ac2a4efea0ca40d030
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-20 17:02:06 +0200

    Add InputView to RegisterView

--- FirebaseAuthentication/FirebaseAuthentication/Core/Authentication/RegisterView.swift
+++ FirebaseAuthentication/FirebaseAuthentication/Core/Authentication/RegisterView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var email: String = ""
+    @State private var fullname: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+
     var body: some View {
         VStack {
             Image(.firebaseLogo)
@@ -15,6 +20,32 @@ struct RegisterView: View {
                 .scaledToFit()
                 .frame(width: 120)
                 .padding(.vertical, 30)
+            
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "johndoe@protonmail.com")
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled(true)
+                .keyboardType(.emailAddress)
+                
+                InputView(text: $fullname,
+                          title: "Full Name",
+                          placeholder: "John Doe")
+                .autocorrectionDisabled(true)
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm your password",
+                          isSecureField: true)
+            }
+            .padding(.horizontal, 16)
+            .padding(.top, 12)
             
             Spacer()
         }
