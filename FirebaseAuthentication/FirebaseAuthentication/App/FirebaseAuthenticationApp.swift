a43db7dd0ee4364bbe59c23b585814624a265f76
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-24 23:11:13 +0200

    Add Firebase config to main App

--- FirebaseAuthentication/FirebaseAuthentication/App/FirebaseAuthenticationApp.swift
+++ FirebaseAuthentication/FirebaseAuthentication/App/FirebaseAuthenticationApp.swift
@@ -5,10 +5,22 @@
 //  Created by Marc on 17/06/2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct FirebaseAuthenticationApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @StateObject var viewModel = AuthViewModel()
     
     var body: some Scene {
