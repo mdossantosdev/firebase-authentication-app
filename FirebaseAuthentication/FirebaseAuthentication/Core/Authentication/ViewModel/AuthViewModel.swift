84d63aed8095b5a8444e6f634152b2bc6f45d691
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-26 23:09:49 +0200

    Add fetchUser method to AuthViewModel

--- FirebaseAuthentication/FirebaseAuthentication/Core/Authentication/ViewModel/AuthViewModel.swift
+++ FirebaseAuthentication/FirebaseAuthentication/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -14,18 +14,34 @@ class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
     
-    init() {}
+    let db = Firestore.firestore()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
+    }
     
     func register(withEmail email: String, password: String, fullname: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
-            userSession = result.user
+            self.userSession = result.user
             let user = User(id: result.user.uid, fullname: fullname, email: email)
             guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
             
-            try await Firestore.firestore().collection("user").document(user.id).setData(encodedUser)
+            try await db.collection("users").document(user.id).setData(encodedUser)
         } catch {
             print("DEBUG: Failed to create user with error \(error.localizedDescription)")
         }
+    }
+    
+    func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try? await db.collection("users").document(uid).getDocument()
+        guard let user = try? snapshot?.data(as: User.self) else { return }
+        self.currentUser = user
+        print("\(String(describing: currentUser))")
     }
 }
