704592629bfb122e740f088bea888b1ee0450b3d
Marc Dos Santos <mdossantosdev@gmail.com>
2024-06-26 23:31:25 +0200

    Add AuthViewModel to ProfileView and replace MOCK_USER with real user data

--- FirebaseAuthentication/FirebaseAuthentication/Core/Profile/ProfileView.swift
+++ FirebaseAuthentication/FirebaseAuthentication/Core/Profile/ProfileView.swift
@@ -8,78 +8,82 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List {
-            Section {
-                HStack(spacing: 12) {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray2))
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 6) {
-                        Text(User.MOCK_USER.fullname)
-                            .font(.subheadline)
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack(spacing: 12) {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundStyle(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray2))
+                            .clipShape(Circle())
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
-                            .tint(.gray)
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text(user.fullname)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .tint(.gray)
+                        }
                     }
                 }
-            }
-            
-            Section("General") {
-                SettingsRowView(imageName: "gear",
-                                title: "App Version",
-                                titleColor: nil,
-                                tintColor: Color(.systemGray),
-                                text: "1.0.0")
-            }
-            
-            Section("Device") {
-                SettingsRowView(imageName: "iphone",
-                                title: "Device Name",
-                                titleColor: nil,
-                                tintColor: Color(.systemGray),
-                                text: UIDevice.current.name)
                 
-                SettingsRowView(imageName: "cpu",
-                                title: "System Name",
-                                titleColor: nil,
-                                tintColor: Color(.systemGray),
-                                text: UIDevice.current.systemName)
-                
-                SettingsRowView(imageName: "number",
-                                title: "System Version",
-                                titleColor: nil,
-                                tintColor: Color(.systemGray),
-                                text: UIDevice.current.systemVersion)
-            }
-            
-            Section("Account") {
-                Button {
-                    print("LOG OUT")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle",
-                                    title: "Log Out",
+                Section("General") {
+                    SettingsRowView(imageName: "gear",
+                                    title: "App Version",
                                     titleColor: nil,
-                                    tintColor: .red,
-                                    text: nil)
+                                    tintColor: Color(.systemGray),
+                                    text: "1.0.0")
                 }
                 
-                Button {
-                    print("DELETE")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle",
-                                    title: "Delete Account",
-                                    titleColor: .red,
-                                    tintColor: .red,
-                                    text: nil)
+                Section("Device") {
+                    SettingsRowView(imageName: "iphone",
+                                    title: "Device Name",
+                                    titleColor: nil,
+                                    tintColor: Color(.systemGray),
+                                    text: UIDevice.current.name)
+                    
+                    SettingsRowView(imageName: "cpu",
+                                    title: "System Name",
+                                    titleColor: nil,
+                                    tintColor: Color(.systemGray),
+                                    text: UIDevice.current.systemName)
+                    
+                    SettingsRowView(imageName: "number",
+                                    title: "System Version",
+                                    titleColor: nil,
+                                    tintColor: Color(.systemGray),
+                                    text: UIDevice.current.systemVersion)
+                }
+                
+                Section("Account") {
+                    Button {
+                        print("LOG OUT")
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle",
+                                        title: "Log Out",
+                                        titleColor: nil,
+                                        tintColor: .red,
+                                        text: nil)
+                    }
+                    
+                    Button {
+                        print("DELETE")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle",
+                                        title: "Delete Account",
+                                        titleColor: .red,
+                                        tintColor: .red,
+                                        text: nil)
+                    }
                 }
             }
         }
